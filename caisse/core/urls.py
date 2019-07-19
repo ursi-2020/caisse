7fda94e0b6a9a999d8ab30ed1261d50599e2442a
Alexandre Buhl <alexandre.buhl@epita.fr>
2019-07-19 22:17:39 +0200

    Setup socle

--- caisse/core/urls.py
+++ caisse/core/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('application.caisse.urls')),
-    path('caisse/', include('caisse.urls')),
+    path('', include('application.djangoapp.urls')),
     path('admin/', admin.site.urls),
 ]
