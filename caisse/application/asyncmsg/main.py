3f09177ef441780c58f9bb3352f1e015e6508c4b
Romain Chevennement <romain.chevennement@epita.fr>
2019-07-21 13:52:33 +0200

    [Caisse] recréation

--- caisse/application/asyncmsg/main.py
+++ caisse/application/asyncmsg/main.py
@@ -6,6 +6,8 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 django.setup()
 
+from apipkg import queue_manager as queue
+
 from application.djangoapp.models import *
 
 
@@ -14,6 +16,9 @@ def main():
     for v in Vente.objects.all():
         print("ID: " + str(v.id) + "\tArticle: " + v.article.nom + "\tDate: " + str(v.date))
 
+def callback(ch, method, properties, body):
+    print(" [x] Received from queue %r" % body)
 
 if __name__ == '__main__':
     main()
+    queue.receive('AppB', callback)
--- caisse/application/djangoapp/urls.py
+++ caisse/application/djangoapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('helloworld', views.helloworld, name='helloworld')
+urlpatterns=[
+    path('',views.index,name='index'),
+    path('helloworld',views.helloworld,name='helloworld')
 ]
